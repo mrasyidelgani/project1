0e1939ffd67a06736ac1e97a4f58c8c855d604ca
mrasyidelgani@gmail <mrasyidelgani@gmail.com>
2018-01-14 06:53:13 -0800

    uas

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -1,85 +1,43 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Author;
 use Illuminate\Http\Request;
-
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+   public function index()
+   {
+      $authors = Author::all();
+      return view('author.index', compact('authors'));
+   }
+   public function create()
+   {
+      return view('author.create');
+      // resources / views / category / create.blade.php
+   }
+   public function store(Request $request)
+   {
+      Author::create($request->all());
+      return redirect('author');
+   }
+   public function edit(Author $author)
     {
-        //
+     return view('author.edit', compact('author'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+ 
+     public function show(Author $author)
     {
-        //
+     return view('author.show', compact('author'));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function update(Request $request, Author $author)
     {
-        //
+     $author->update($request->all());
+     return redirect('author');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Author $author)
+ 
+    public function destroy(Request $request, Author $author)
     {
-        //
+     $author->delete();
+     return redirect('author');
     }
 }
